76aa19d45c54a2fd77f8075a484f00fd6a305388
Akhil Pinnamaraju <Akhil Pinnamaraju>
2016-09-17 23:14:04 +0100

    my first commit

---
+++ com.tests.ver1/src/test/java/com/tests/AppTest.java
@@ -0,0 +1,25 @@
+package com.tests;
+
+
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    
+	@Test
+	public void openBrowser(){
+		
+		System.out.println("Hi There how are you?");
+			
+	}
+
+    
+    
+}
